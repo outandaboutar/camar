c3a5c77bf6ba1df7aab529b676eb4b31eb2a3591
outandaboutar <80853233+outandaboutar@users.noreply.github.com>
2021-05-15 18:08:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function renderPlaces(places) {
         let longitude = place.location.lng;
 
         let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};look-at="[gps-camera]"`);
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
         setModel(models[modelIndex], model);
 
